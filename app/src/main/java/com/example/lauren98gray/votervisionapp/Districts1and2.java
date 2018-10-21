dc7f397559fb5188437796f73d3e54ba177794b4
po-girl <mbolds@xula.edu>
2018-10-20 22:57:51 -0400

    Add District1 and District2

---
+++ app/src/main/java/com/example/lauren98gray/votervisionapp/Districts1and2.java
@@ -0,0 +1,13 @@
+package com.example.lauren98gray.votervisionapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Districts1and2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.candidate_activity);
+    }
+}
